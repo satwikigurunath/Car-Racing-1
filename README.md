3b3430575c67cc5b959cc7dc1bd9b46a4e695ea5
satwikigurunath <76837776+satwikigurunath@users.noreply.github.com>
2021-05-05 20:42:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Car-Racing-1
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,45 @@
+var hypnoticBall;
+var database,position
+
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    hyponoticBall = createSprite(250,250,10,10);
+    hyponoticBall.shapeColor = "red";
+    var hyponoticBallPosition=database.ref("ball/position")
+    hyponoticBallPosition.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+   database.ref("ball/position").set({
+       "x":position.x+x,
+       "y":position.y+y
+   })
+}
+
+function readPosition(data){
+    position=data.val()
+    hyponoticBall.x=position.x
+    hyponoticBall.y=position.y
+}
+
+function showError(){
+    console.log("error")
+}
